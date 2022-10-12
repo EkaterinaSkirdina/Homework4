d22ba4a2b129e6bd3d7627e17ed36decaf8c3eb7
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2022-10-12 19:07:19 +0300

    Task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход
+два числа (A и B) и возводит число A в натуральную
+степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16          */
+
+Console.Clear();
+
+Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число В: ");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine(ExpNum(A, B));
+
+double ExpNum(int x, int y)
+{
+    double expon = Math.Pow(x, y);
+    return expon;
+}
