7bf2494c0045d7e19967638e606db2c6ed64b0e6
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2022-10-13 16:32:44 +0300

    Task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив
+из 8 элементов и выводит их на экран. */
+
+Console.Clear();
+
+int[] array = new int[8];
+FillArray(array);
+PrintArray(array);
+
+void FillArray(int[] arg1)
+{
+    for (int i = 0; i < arg1.Length; i++)
+    {
+        arg1[i] = new Random().Next(0, 100);
+    }
+}
+
+void PrintArray(int[] arg2)
+{
+    Console.Write("[");
+    for (int i = 0; i < arg2.Length-1; i++)
+    {
+        Console.Write($"{arg2[i]}, ");
+    }
+    Console.Write($"{arg2.Length-1}]");
+}
